f024e6359cd5e635bb0ce3c5206900e891054faf
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-25 15:37:26 -0300

    feat(api): create product api interface

---
+++ src/main/java/br/com/productservice/controller/ProductApi.java
@@ -0,0 +1,24 @@
+package br.com.productservice.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+
+import br.com.productservice.common.model.ErrorResponse;
+import br.com.productservice.model.payload.ProductPayload;
+import br.com.productservice.model.response.ProductResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(tags = "ProductApi")
+public interface ProductApi {
+
+	@ApiOperation("Create media")
+	@ApiResponses({ @ApiResponse(code = 201, message = "Product was created with success"),
+			@ApiResponse(code = 400, message = "Wrong information was sent", response = ErrorResponse.class),
+			@ApiResponse(code = 500, message = "An unexpected error occurred", response = ErrorResponse.class) })
+	ResponseEntity<ProductResponse>  create(@Valid ProductPayload payload);
+
+}
