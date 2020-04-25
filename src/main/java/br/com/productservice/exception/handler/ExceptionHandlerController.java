ee655fadd379c1a95ca4a2b41abe5daa3d067220
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-25 16:32:16 -0300

    feat(exception): adding method to throw exception when product not exist

--- src/main/java/br/com/productservice/exception/handler/ExceptionHandlerController.java
+++ src/main/java/br/com/productservice/exception/handler/ExceptionHandlerController.java
@@ -1,8 +1,17 @@
 package br.com.productservice.exception.handler;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.productservice.exception.ProductNotExistsException;
 
 @RestControllerAdvice
 public class ExceptionHandlerController {
+	
+    @ExceptionHandler(ProductNotExistsException.class)
+    public ResponseEntity<Object> handleProductNotExistsException() {
+        return ResponseEntity.noContent().build();
+    }
 
 }
