635b8a3bedd29edab705b7c56e548cabbb171e2d
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-25 16:31:36 -0300

    feat(payload): adding annotations to validate fields

--- src/main/java/br/com/productservice/model/payload/ProductPayload.java
+++ src/main/java/br/com/productservice/model/payload/ProductPayload.java
@@ -1,5 +1,9 @@
 package br.com.productservice.model.payload;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import br.com.productservice.model.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,8 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductPayload {
 	
+	@NotNull(message = "{ProductPayload.category.notNull}")
 	private Category category;
 	
+	@NotBlank(message = "{ProductPayload.name.notBlank}")
+	@Size(min = 5, max = 100, message = "{ProductPayload.name.invalidSize}")
 	private String name;
 	
 }
