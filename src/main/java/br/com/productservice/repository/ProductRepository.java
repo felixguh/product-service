939c29f9b155c62882540296c4699000e04ec390
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-25 16:22:05 -0300

    feat(repository): adding method to find product by product number

--- src/main/java/br/com/productservice/repository/ProductRepository.java
+++ src/main/java/br/com/productservice/repository/ProductRepository.java
@@ -1,12 +1,16 @@
 package br.com.productservice.repository;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.productservice.model.Product;
 
 @Repository
 public interface ProductRepository extends MongoRepository<Product, ObjectId>{
+	
+	Optional<Product> findByProductNumber(final Long productNumber);
 
 }
