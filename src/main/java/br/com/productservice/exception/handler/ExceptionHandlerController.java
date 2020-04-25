b60b2cc75b864f8e762ba5e7d03478fee3ab5a05
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-25 14:06:46 -0300

    feat(exception): create handler

---
+++ src/main/java/br/com/productservice/exception/handler/ExceptionHandlerController.java
@@ -0,0 +1,8 @@
+package br.com.productservice.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+}
