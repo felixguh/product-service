d05b4f8e26e6db66a83d606e9452aa61f379917d
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 12:47:54 -0300

    feat(model): adding price attribute

--- src/main/java/br/com/productservice/model/response/ProductResponse.java
+++ src/main/java/br/com/productservice/model/response/ProductResponse.java
@@ -1,5 +1,6 @@
 package br.com.productservice.model.response;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -8,9 +9,11 @@ import br.com.productservice.model.Product;
 import br.com.productservice.model.enums.Category;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @ApiModel(description = "Product data")
 public class ProductResponse {
 	
@@ -30,6 +33,11 @@ public class ProductResponse {
 	@ApiModelProperty(value = "Product name", example = "POTATO")
 	public String getName() {
 		return entity.getName();
+	}
+	
+	@ApiModelProperty(value = "Product price", example = "15.50")
+	public BigDecimal getPrice() {
+		return entity.getPrice();
 	}
 	
 	@ApiModelProperty(value = "Product creation date", example = "2020-04-25T15:53:46.673577")
